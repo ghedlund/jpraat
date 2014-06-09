19a4044369b806aa8f98a32140e2c1b1d87fa916
Greg Hedlund <ghedlund@mun.ca>
2014-06-09 16:13:52 -0230

    Updated win32-amd64 lib

--- src/main/cpp/sys/praatlib.h
+++ src/main/cpp/sys/praatlib.h
@@ -7,9 +7,17 @@
 #define _praatlib_h
 
 #ifdef WIN32
+#ifdef __cplusplus
 #define PRAAT_LIB_EXPORT extern "C" __declspec( dllexport )
 #else
+#define PRAAT_LIB_EXPORT __declspec( dllexport )
+#endif
+#else
+#ifdef __cplusplus
 #define PRAAT_LIB_EXPORT extern "C"
+#else
+#define PRAAT_LIB_EXPORT
+#endif
 #endif
 
 #endif
--- src/main/cpp/sys/sendpraat.h
+++ src/main/cpp/sys/sendpraat.h
@@ -1,11 +1,16 @@
 /* sendpraat.h */
 /* Paul Boersma, September 27, 2000 */
 
+
+#ifdef PRAAT_LIB
+#include "praatlib.h"
+#endif
+
 #ifdef __cplusplus
 	extern "C" {
 #endif
 
-char *sendpraat (void *display, const char *programName, long timeOut, const char *text);
+PRAAT_LIB_EXPORT char *sendpraat (void *display, const char *programName, long timeOut, const char *text);
 
 #ifdef __cplusplus
 	}
