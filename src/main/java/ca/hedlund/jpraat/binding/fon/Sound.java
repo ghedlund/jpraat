2470ace572b84b4fad8940f79c8666824d2f413a
Gregory Hedlund <greg.hedlund@gmail.com>
2014-09-25 08:30:26 -0230

    Added checks to input parameters to Sound_to_XXX methods

--- src/main/java/ca/hedlund/jpraat/binding/fon/Sound.java
+++ src/main/java/ca/hedlund/jpraat/binding/fon/Sound.java
@@ -61,10 +61,40 @@ public class Sound extends Vector {
 		return Praat.INSTANCE.Sound_extractPart(this, t1, t2, windowShape, relativeWidth, preserveTimes);
 	}
 
+	/**
+	 * Create a new spectrogram object from this sound.
+	 * 
+	 * 
+	 * 
+	 * @param effectiveAnalysisWidth
+	 * @param fmax
+	 * @param minimumTimeStep1
+	 * @param minimumFreqStep1
+	 * @param windowShape
+	 * @param maximumTimeOversampling
+	 * @param maximumFreqOversampling
+	 * @return
+	 * 
+	 * @throws IllegalArgumentException
+	 */
 	@Declared("fon/Sound_to_Spectrogram.h")
 	public Spectrogram toSpectrogram (double effectiveAnalysisWidth, double fmax,
 			double minimumTimeStep1, double minimumFreqStep1, kSound_to_Spectrogram_windowShape windowShape,
 			double maximumTimeOversampling, double maximumFreqOversampling) {
+		
+		// do some checks so here so we don't throw MelderErrors and crash the JVM
+		// from praat:fon/Sound_and_Spectrogram.cpp#Sound_to_Spectrogram
+		double physicalAnalysisWidth =
+				windowShape == kSound_to_Spectrogram_windowShape.GAUSSIAN ? 2 * effectiveAnalysisWidth : effectiveAnalysisWidth;
+		double duration = getDx() * getNx();
+		
+		long nsamp_window = (long)Math.floor(physicalAnalysisWidth / getDx());
+		if(nsamp_window < 1) 
+			throw new IllegalArgumentException("Your analysis window is too short: less than two samples.");
+		if(physicalAnalysisWidth > duration) {
+			throw new IllegalArgumentException("Your sound is too short.");
+		}
+		
 		return Praat.INSTANCE.Sound_to_Spectrogram(this, effectiveAnalysisWidth, fmax, minimumTimeStep1, minimumFreqStep1, 
 				windowShape, maximumTimeOversampling, maximumFreqOversampling);
 	}
@@ -82,6 +112,8 @@ public class Sound extends Vector {
 		double periodsPerWindow, int maxnCandidates, int accurate,
 		double silenceThreshold, double voicingThreshold, double octaveCost,
 		double octaveJumpCost, double voicedUnvoicedCost, double maximumPitch) {
+		checkMaxnCandidates(maxnCandidates);
+		checkMinPitchPeriodsPerWindow(minimumPitch, periodsPerWindow);
 		return Praat.INSTANCE.Sound_to_Pitch_ac(this, timeStep, minimumPitch, periodsPerWindow, maxnCandidates, accurate, 
 				silenceThreshold, voicingThreshold, octaveCost, octaveJumpCost, voicedUnvoicedCost, maximumPitch);
 	}
@@ -92,6 +124,8 @@ public class Sound extends Vector {
 		double periodsPerWindow, int maxnCandidates, int accurate,
 		double silenceThreshold, double voicingThreshold, double octaveCost,
 		double octaveJumpCost, double voicedUnvoicedCost, double maximumPitch) {
+		checkMaxnCandidates(maxnCandidates);
+		checkMinPitchPeriodsPerWindow(minimumPitch, periodsPerWindow);
 		return Praat.INSTANCE.Sound_to_Pitch_cc(this, timeStep, minimumPitch, periodsPerWindow, maxnCandidates, accurate,
 				silenceThreshold, voicingThreshold, octaveCost, octaveJumpCost, voicedUnvoicedCost, maximumPitch);
 	}
@@ -146,37 +180,90 @@ public class Sound extends Vector {
 		double voicedUnvoicedCost, /* default 0.14 */
 		double maximumPitch)     /* (Hz) */
 	{
+		checkMaxnCandidates(maxnCandidates);
+		checkMinPitchPeriodsPerWindow(minimumPitch, periodsPerWindow);
 		return Praat.INSTANCE.Sound_to_Pitch_any(this, dt, minimumPitch, periodsPerWindow, maxnCandidates, method, 
 				silenceThreshold, voicingThreshold, octaveCost, octaveJumpCost, voicedUnvoicedCost, maximumPitch);
 	}
 	
+	private void checkMaxnCandidates(int maxnCandidates) {
+		if(maxnCandidates < 2)
+			throw new IllegalArgumentException("Max candidates must be >= 2");
+	}
+	
+	private void checkMinPitchPeriodsPerWindow(double minPitch, double periodsPerWindow) {
+		double duration = getNx() * getDx();
+		if(minPitch < periodsPerWindow / duration) {
+			final String msg = 
+					"To analyse this sound minimum pitch must be no less than "
+					+ periodsPerWindow/duration + " Hz.";
+			throw new IllegalArgumentException(msg);
+		}
+		
+		double dt_window = periodsPerWindow / minPitch;
+		long nsamp_window = (long)Math.floor(dt_window / getDx());
+		long halfnsamp_window = nsamp_window / 2 - 1;
+		if(halfnsamp_window < 2) {
+			throw new IllegalArgumentException("Analysis window too short.");
+		}
+	}
+
 	/*
 		Which = 1: Burg.
 		Which = 2: Split-Levinson
 	*/
 	public Formant to_Formant_any (double timeStep, int numberOfPoles, double maximumFrequency,
 			double halfdt_window, int which, double preemphasisFrequency, double safetyMargin) {
+		checkFormantWindow(numberOfPoles, halfdt_window);
 		return Praat.INSTANCE.Sound_to_Formant_any(this, timeStep, numberOfPoles, maximumFrequency, halfdt_window, which, preemphasisFrequency, safetyMargin);
 	}
 	
 	/* Throws away all formants below 50 Hz and above Nyquist minus 50 Hz. */
-	public Formant to_Formant_burg (double timeStep, double maximumNumberOfFormants,
+	public Formant to_Formant_burg (double timeStep, double numberOfFormants,
 		double maximumFormantFrequency, double windowLength, double preemphasisFrequency) {
-		return Praat.INSTANCE.Sound_to_Formant_burg(this, timeStep, maximumNumberOfFormants, maximumFormantFrequency, windowLength, preemphasisFrequency);
+		checkFormantWindow((int)(2*numberOfFormants), windowLength);
+		return Praat.INSTANCE.Sound_to_Formant_burg(this, timeStep, numberOfFormants, maximumFormantFrequency, windowLength, preemphasisFrequency);
 	}
 
 	/* Same as previous, but keeps all formants. Good for resynthesis. */
-	public Formant to_Formant_keepAll (double timeStep, double maximumNumberOfFormants,
+	public Formant to_Formant_keepAll (double timeStep, double numberOfFormants,
 		double maximumFormantFrequency, double windowLength, double preemphasisFrequency) {
-		return Praat.INSTANCE.Sound_to_Formant_keepAll(this, timeStep, maximumNumberOfFormants, maximumFormantFrequency, windowLength, preemphasisFrequency);
+		checkFormantWindow((int)(2*numberOfFormants), windowLength);
+		return Praat.INSTANCE.Sound_to_Formant_keepAll(this, timeStep, numberOfFormants, maximumFormantFrequency, windowLength, preemphasisFrequency);
 	}
 
 	public Formant to_Formant_willems (double timeStep, double numberOfFormants,
 		double maximumFormantFrequency, double windowLength, double preemphasisFrequency) {
+		checkFormantWindow((int)(2*numberOfFormants), windowLength);
 		return Praat.INSTANCE.Sound_to_Formant_willems(this, timeStep, numberOfFormants, maximumFormantFrequency, windowLength, preemphasisFrequency);
 	}
 	
+	private void checkFormantWindow(int numberOfPoles, double windowLength) {
+		double dt_window = 2.0 * windowLength;
+		long nsamp_window = (long)Math.floor(dt_window/getDx());
+		
+		if(nsamp_window < numberOfPoles + 1) {
+			throw new IllegalArgumentException("Window too short.");
+		}
+	}
+	
 	public Intensity to_Intensity (double minimumPitch, double timeStep, int subtractMean) {
+
+		if(Double.isNaN(minimumPitch) || minimumPitch <= 0.0) {
+			throw new IllegalArgumentException("Minimum pitch must be > 0.0");
+		}
+		
+		if(Double.isNaN(timeStep) || timeStep < 0.0) {
+			throw new IllegalArgumentException("Time step must be >= 0.0");
+		}
+		
+		double duration = getDx() * getNx();
+		double minDuration = 6.4 / minimumPitch;
+		if(duration < minDuration) {
+			throw new IllegalArgumentException("The duration of the sound in an intensity analysis should be "
+					+ "at least 6.4 divided by the minimum pitch.  i.e., at least " + minDuration + "s.");
+		}
+		
 		return Praat.INSTANCE.Sound_to_Intensity(this, minimumPitch, timeStep, subtractMean);
 	}
 	
